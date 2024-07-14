1ee8aa127152e0968fe445036a922131d02f5799
kim-do-kyun <zetgi7dk5359@gmail.com>
2024-07-15 02:34:10 +0900

    new build

--- auto.bat
+++ auto.bat
@@ -1,14 +1,34 @@
 @echo off
-cd C:\대학자료\KSSoftware
+
 gradlew bootJar
-cd src/front npm run build
-move build KSSoftware_Test_Download
+
+cd src/front 
+
+dir
+
+npm run build
+
+cd C:\KSSoftware_Test_Download
+
+dir
+
+del *.jar
+rmdir build
+
+cd C:\대학자료\KSSoftware\src\front
+
+dir
+
+move build C:\KSSoftware_Test_Download
+
 cd C:\대학자료\KSSoftware\build\libs
-move KS*.jar KSSoftware_Test_Download
+
+dir
+
+move KS*.jar C:\KSSoftware_Test_Download
 
 git add .
 git commit -m "new build"
 git push
 
 ssh -i "C:\KSSoftware_Test_Download\KSSoftwareTest_Keypair.pem" ubuntu@ec2-52-64-204-202.ap-southeast-2.compute.amazonaws.com
-
