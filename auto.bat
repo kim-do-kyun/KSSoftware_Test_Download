fc76caa611dbd767dbdf70734c1d6b143ab1ec95
kim-do-kyun <zetgi7dk5359@gmail.com>
2024-07-14 21:36:12 +0900

    new build

--- auto.bat
+++ auto.bat
@@ -3,9 +3,8 @@ git add .
 git commit -m "new build"
 git push
 
-ssh -i "C:\KSSoftware_Test_Download\KSSoftwareTest_Keypair.pem" ubuntu@ec2-52-64-204-202.ap-southeast-2.compute.amazonaws.com
+ssh -i BatchMode=yes "C:\KSSoftware_Test_Download\KSSoftwareTest_Keypair.pem" ubuntu@ec2-52-64-204-202.ap-southeast-2.compute.amazonaws.com
 
-timeout /t 5
 
 cd ~
 cd /usr/local
