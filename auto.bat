86ba9fda588c8613cd8745838c6ef8eaaa5c4dad
kim-do-kyun <zetgi7dk5359@gmail.com>
2024-07-14 21:12:53 +0900

    new build

--- auto.bat
+++ auto.bat
@@ -2,3 +2,11 @@
 git add .
 git commit -m "new build"
 git push
+
+ssh -i "C:\대학자료\KSSoftware_Test_Download\KSSoftwareTest_Keypair.pem" ubuntu@ec2-52-64-204-202.ap-southeast-2.compute.amazonaws.com
+cd ~
+cd /usr/local
+sudo rm -rf app
+sudo git clone https://github.com/kim-do-kyun/KSSoftware_Test_Download.git app
+cd app
+sudo java -jar K*.jar
