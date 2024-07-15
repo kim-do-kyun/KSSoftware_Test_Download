98927b0e9bef91f2120b1b68543484228422ff57
kim-do-kyun <zetgi7dk5359@gmail.com>
2024-07-15 14:13:37 +0900

    Upload Build

--- autoTest.bat
+++ autoTest.bat
@@ -1,15 +1,20 @@
-cd C:\KSSoftware_Test_Download
+cd C:/KSSoftware_Test_Download
 del *.jar
-
 echo "jar파일 삭제"
 
 cd c:/대학자료/KSSoftware
 call gradlew bootJar
-move build/libs/*.jar C:\KSSoftware_Test_Download
+echo "그래들 빌드 완료"
+
+move build/libs/K*.jar C:/KSSoftware_Test_Download
+echo "jar파일 이동 완료"
 
 cd src/front
 call npm run build
+echo "run build 완료"
+
 move build /./KSSoftware_Test_Download
+echo "build파일 이동완료"
 
 cd /./KSSoftware_Test_Download
 git add .
